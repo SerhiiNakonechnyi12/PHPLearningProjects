1eb41ed8ca9bf35be8a268c0a95ee8f8e022e5be
serhii12 <serhii19911204@gmail.com>
2020-12-16 21:56:50 +0200

    add CW

---
+++ Practice/CW_003.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <form method="POST">
+        <div>R<input type='text' size='40' name="r"></div>
+        <div>G<input type='text' size='40' name="g"></div>
+        <div>B<input type='text' size='40' name="b"></div>
+        <div>
+            <button type="submit" name="accept">Accept</button>
+        </div>
+    </form>
+</body>
+</html>
+<?php
+
+if (isset($_POST["accept"])) {
+    $r = $_POST["r"];
+    $g = $_POST["g"];
+    $b = $_POST["b"];
+    echo "<span style='color: rgb(".$r.",".$g.",".$b.")'>Change text color</span>";
+}
+?>
+
+
+<!-- // Задание.
+// Написать PHP скрипт, создающий на странице три текстовых поля. В эти
+// поля пользователь должен заносить значения R, G и B цветовых компонент (в
+// интервале 0-255). На странице также должна присутствовать кнопка Accept и тег
+// span с каким-либо текстом внутри.
+// После нажатия на кнопку Accept, надо создать цвет на основе введенных
+// пользователем значений R, G и B. Этим цветом залить фон тега span, а текст
+// залить дополнительным цветом. -->
