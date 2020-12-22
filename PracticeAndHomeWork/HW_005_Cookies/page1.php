20bcd727412bd82f29a0eef3a4e82382f1cfc044
serhii19911204@gmail.com <serhii19911204@gmail.com>
2020-12-22 16:58:01 +0200

    Add complete hw_005

---
+++ PracticeAndHomeWork/HW_005_Cookies/page1.php
@@ -0,0 +1,51 @@
+<?
+// Тема: Куки, сессии 
+// Задание.
+// Инициализируйте переменную для подсчета количества посещений, хранения 
+// вашего имени и даты последнего посещения. 
+// Если соответствующие данные передавались через куки, сохраните их в 
+// эту переменную. Нарастите счетчик посещений, 
+// измените дату на текущую. Установите соответствующие куки. 
+// Выводите информацию о количестве посещений и 
+// дате последнего посещения.
+// Для проверки корректности работы счетчика напишите еще один скрипт, 
+// например clearCookie.php для 
+// сброса всех данных, хранящихся в куки.
+
+
+// Cookie
+$counter = isset($_COOKIE['counter']) ? $_COOKIE['counter'] : 0;
+$counter++;
+setcookie("counter", $counter);
+
+$timeCookie = isset($_COOKIE['timeCookie']) ? $_COOKIE['timeCookie'] : 0;
+$timeCookie = date("D, d M Y H:i:s");
+setcookie("timeCookie", $timeCookie);
+
+$userName = "Serhii";
+
+setcookie($counter, time() - 3600);
+
+// Сессии
+session_start();
+$_SESSION["time"] = date("D, d M Y H:i:s");
+// echo $_SESSION["time"];
+
+$_SESSION["name"] = "Serhii";
+
+// $_SESSION["counter"] = isset($_COOKIE['counter']) ? $_COOKIE['counter'] : 0;
+//$_SESSION++;
+
+echo (
+    "Посещений страницы: ".$counter. "<br/>
+    Имя посетителя: ".$userName."<br/>
+    Дата последнего посещения: ".$timeCookie. "<br/><br/>"
+    
+    // "Посещений страницы: ".$_SESSION["counter"]. "<br/>
+    // "Имя: ".$_SESSION["name"] "<br/>
+    // Дата последнего посещения: ".$_SESSION["time"]
+    
+);
+?>
+
+
