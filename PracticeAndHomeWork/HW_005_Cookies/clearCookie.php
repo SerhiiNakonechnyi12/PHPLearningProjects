041991dd34b84753a6c94433f087c83ccd39c815
serhii19911204@gmail.com <serhii19911204@gmail.com>
2020-12-22 20:44:35 +0200

    add last update of HW_005

--- PracticeAndHomeWork/HW_005_Cookies/clearCookie.php
+++ PracticeAndHomeWork/HW_005_Cookies/clearCookie.php
@@ -1,3 +1,13 @@
 <?
 setcookie("counter", $counter, time()-10);
 setcookie("timeCookie", $counter, time()-10);
+echo (
+"Cookies was delete"
+);
+
+echo (
+    "Посещений страницы: ".$counter. "<br/>
+    Имя посетителя: ".$userName."<br/>
+    Дата последнего посещения: ".$timeCookie. "<br/><br/>"
+    
+);
