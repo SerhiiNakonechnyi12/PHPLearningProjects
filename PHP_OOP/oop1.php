1d6615d7f02126626981a33288363040ad406cda
serhii19911204@gmail.com <serhii19911204@gmail.com>
2021-01-18 16:52:02 +0200

    test

--- PHP_OOP/oop1.php
+++ PHP_OOP/oop1.php
@@ -3,8 +3,20 @@ class Point{
     public $x;
     public $y;
 
+    function Show(){
+        echo "<br/>X = ". $this->x .", Y = ". $this->y ."<br/>";
+    }
+
+    function __construct($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
 }
 
-$p1 = new Point();
+$p1 = new Point(2, 4);
+// $p1->x = 30;
+// $p1->y = 50;
 
-?>
+var_dump($p1);
+$p1->Show();
