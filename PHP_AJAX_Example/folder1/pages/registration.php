83090a3a58256df19d0ad28d04c3aa6307b2344c
serhii12 <serhii19911204@gmail.com>
2021-01-18 19:22:25 +0200

    add ajax examples

---
+++ PHP_AJAX_Example/folder1/pages/registration.php
@@ -0,0 +1,32 @@
+<h3>Registration</h3>
+<?php
+if(!isset($_POST["regbtn"])){
+    ?>
+    <form action="index.php?page=3" method="POST">
+    <div class="form-group">
+    <label for="login">Login:</label>
+    <input type="text" name="login" class="form-control">
+    </div>
+    <div class="form-group">
+    <label for="pass1">Password:</label>
+    <input type="password" name="pass1" class="form-control">
+    </div>
+    <div class="form-group">
+    <label for="pass2">Confirm Password:</label>
+    <input type="password" name="pass2" class="form-control">
+    </div>
+    <div class="form-group">
+    <label for="email">Password:</label>
+    <input type="email" name="email" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary" name="regbtn">Register</button>
+    </form>
+    <?php
+}
+else
+{
+    if(register($_POST["login"], $_POST["pass1"], $_POST["email"])){
+        echo "<h3/><span style='color: green'>Пользователь ".$_POST["login"]." добавлен!</span><h3/>";
+    }
+}
+?>
---
+++ PHP_AJAX_Example/folder2/ajax.php
@@ -0,0 +1,10 @@
+<?php
+$name = $_GET["name"];
+$name = strtolower($name);
+$arr = ["Mario", "Maxim", "Milana", "Mila", "Marcos", "Mark", "Michael"];
+$response = "";
+foreach($arr as $n){
+    if(substr(strtolower($n), 0, strlen($name))==$name)
+    $response.= $n ."<br>";
+}
+echo $response;
