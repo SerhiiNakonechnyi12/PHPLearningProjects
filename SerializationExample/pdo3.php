73c9be100dc565e2151b45b3d5f287306d189c67
serhii19911204@gmail.com <serhii19911204@gmail.com>
2021-01-20 15:54:28 +0200

    remote html tags

--- SerializationExample/pdo3.php
+++ SerializationExample/pdo3.php
@@ -34,19 +34,3 @@ while($row = $ps4->fetch(PDO::FETCH_ASSOC)){
 echo json_encode($arr);
 
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<script type="text/javascript" src="">
-
-</script>
-    
-</body>
-</html>
