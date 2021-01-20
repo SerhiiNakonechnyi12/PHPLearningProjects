f6152fef03f96b12868f022d044fd263e5f31320
serhii19911204@gmail.com <serhii19911204@gmail.com>
2021-01-20 16:35:41 +0200

    add unserialize example

--- Serialization/HW/unserialize.php
+++ Serialization/HW/unserialize.php
@@ -1,24 +1,25 @@
 <?php
-class Picture{
-    public $id;
-    private $name;
-    private $size;
-    private $imagepath;
-    function __construct($id, $name, $size, $imagepath)
-    {
-        $this->id = $id;
-        $this->name = $name;    
-        $this->size = $size;    
-        $this->imagepath = $imagepath;    
-    }
+// class Picture{
+//     public $id;
+//     private $name;
+//     private $size;
+//     private $imagepath;
+//     function __construct($id, $name, $size, $imagepath)
+//     {
+//         $this->id = $id;
+//         $this->name = $name;    
+//         $this->size = $size;    
+//         $this->imagepath = $imagepath;    
+//     }
 
-    function Show(){
-        echo "id: " . $this->id .", Name: ". $this->name."<br>
-        Size: " . $this->size .", imagepath: ". $this->imagepath."<br>";
-    }
-}
+//     function Show(){
+//         echo "id: " . $this->id .", Name: ". $this->name."<br>
+//         Size: " . $this->size .", imagepath: ". $this->imagepath."<br>";
+//     }
+// }
 
 $str = file_get_contents("serialPicture.txt");
 echo $str ."<br>";
-$p2 = unserialize($str);
-$p2->Show();
+$picture = unserialize($str);
+$picture->Show();
+
