0562a7e5a3b59f932227434e34c025672d1d3e2c
serhii19911204@gmail.com <serhii19911204@gmail.com>
2021-01-28 01:50:59 +0200

    add last changes to codeigniter

--- Stepserver5/application/controllers/Home.php
+++ Stepserver5/application/controllers/Home.php
@@ -3,6 +3,7 @@ class Home extends CI_Controller
 {
     public function index()
     {
-        echo "Привет  мир Codeigniter!";
+        // echo "Привет  мир Codeigniter!";
+        $this->load->view("page1.php");
     }
 }
