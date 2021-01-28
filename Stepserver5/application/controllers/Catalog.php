7a3faff26ea53811e12656f0b07dfb05c7a4e5da
serhii19911204@gmail.com <serhii19911204@gmail.com>
2021-01-28 17:42:01 +0200

    add hw part 2

--- Stepserver5/application/controllers/Catalog.php
+++ Stepserver5/application/controllers/Catalog.php
@@ -4,12 +4,12 @@ class Catalog extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model("home model");
+        $this->load->model("catalog_model");
     }
 
     public function getitems()
     {
         $data["items"] = $this->catalog_model->getItems();
-        $this->load->view("showItems", $data);
+        $this->load->view("showItems.php", $data);
     }
 }
